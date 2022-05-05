f7c9abd609834f4016429a54b6fb7263043a4590
Beatriz Gomes <biaakiddo@gmail.com>
2022-05-05 15:16:42 -0300

    Unified request.js and utils.js. Also, added an environment variable 'api_path'

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,5 +1,25 @@
+const api_path = "http://localhost:8000"; //Add the API path here
+
+function makeRequest(method, path, success, data = null, is_image = false) {
+    token = sessionStorage.getItem("token")
+
+    $.ajax({
+        method,
+        url: api_path + "/api/" + path,
+        processData: !is_image,
+        contentType: is_image ? false : "application/x-www-form-urlencoded; charset=UTF-8",
+        cache: false,
+        enctype: 'multipart/form-data',
+        headers: {
+            "Authorization": `Bearer ${token}`
+        },
+        data,
+        success
+    })
+}
+
 function fullImagePath(partial_path) {
-    return "http://localhost:8000/storage/" + partial_path.substring(6)
+    return api_path + "/storage/" + partial_path.substring(6)
 }
 
 function formatPrice(price) {
