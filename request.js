5069b4d62fafb7c0cecba018a90a92ec7d205b4e
Beatriz Gomes <biaakiddo@gmail.com>
2022-05-05 12:38:01 -0300

    Fixed ajax bug

--- request.js
+++ request.js
@@ -1,11 +1,11 @@
-function makeRequest(method, path, success, data = null) {
+function makeRequest(method, path, success, data = null, is_image = false) {
     token = sessionStorage.getItem("token")
 
     $.ajax({
         method,
         url: `http://localhost:8000/api/${path}`,
-        processData: false,
-        contentType: false,
+        processData: !is_image,
+        contentType: is_image ? false : "application/x-www-form-urlencoded; charset=UTF-8",
         cache: false,
         enctype: 'multipart/form-data',
         headers: {
