51920df4ab1a5aa0c18714a532855f5384f9d967
Beatriz Gomes <biaakiddo@gmail.com>
2022-05-05 14:43:33 -0300

    Improved site style and organization

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,3 +1,8 @@
 function fullImagePath(partial_path) {
     return "http://localhost:8000/storage/" + partial_path.substring(6)
 }
+
+function formatPrice(price) {
+    price = parseFloat(price).toFixed(2)
+    return "R$" + price
+}
